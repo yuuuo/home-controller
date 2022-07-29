ee4f58cdd2461a10030032b19a3215fdee5d7f1f
yuuu <y_9907o@yahoo.co.jp>
2022-07-29 22:49:05 +0900

    sample

--- lambda/src/index.ts
+++ lambda/src/index.ts
@@ -1,7 +1,15 @@
-export const handler = async function (event: any, context: any) {
+import { Context, APIGatewayProxyResult, APIGatewayEvent } from "aws-lambda";
+
+export const lambdaHandler = async (
+  event: APIGatewayEvent,
+  context: Context
+): Promise<APIGatewayProxyResult> => {
+  console.log(`Event: ${JSON.stringify(event, null, 2)}`);
+  console.log(`Context: ${JSON.stringify(context, null, 2)}`);
   return {
     statusCode: 200,
-    headers: {},
-    body: { message: "Hello World!" },
+    body: JSON.stringify({
+      message: "hello world",
+    }),
   };
 };
